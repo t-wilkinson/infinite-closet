2fc5f3df166ba299bd0bd9686d990c6599a5a536
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-05-10 14:25:54 +0000

    [fix] Add production origins

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -5,10 +5,14 @@ module.exports = {
       origin: [
         "http://localhost:3000",
         "http://localhost:1337",
+
         "https://" + process.env.PRODUCTION_DOMAIN,
-        "https://dev." + process.env.PRODUCTION_DOMAIN,
         "https://api." + process.env.PRODUCTION_DOMAIN,
+        "https://admin." + process.env.PRODUCTION_DOMAIN,
+
+        "https://dev." + process.env.PRODUCTION_DOMAIN,
         "https://api.dev." + process.env.PRODUCTION_DOMAIN,
+        "https://admin.dev." + process.env.PRODUCTION_DOMAIN,
       ],
     },
   },
