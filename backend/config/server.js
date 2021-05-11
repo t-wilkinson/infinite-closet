adbe403ada5babe024d5d6437d98e6818daf93f1
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-05-11 10:16:13 -0400

    [fix] Correct development/production ports

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ec27516c3ddbb82286803e7d5308c81b'),
+      secret: env("ADMIN_JWT_SECRET", "ec27516c3ddbb82286803e7d5308c81b"),
     },
   },
 });
