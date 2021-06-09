7cd39373d5951a757ddab03ce0fb3a8a2830728f
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-06-09 13:59:41 +0000

    [merge]

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,10 @@
+console.log('hi')
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  proxy: true,
   admin: {
+    autoOpen: false,
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ec27516c3ddbb82286803e7d5308c81b"),
     },
