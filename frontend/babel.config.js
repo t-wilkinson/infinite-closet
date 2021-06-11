45a20a47c413c2c0f32c832c96cce43516c1387b
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-06-11 14:25:49 +0000

    [bug] Include next preset to babel

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,21 +1,4 @@
 module.exports = {
-  moduleIds: true,
-  getModuleId: (name) => name.replace('@/', 'src/'),
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-  // plugins: [
-  //   [
-  //     'module-resolver',
-  //     {
-  //       extensions: ['.js', '.jsx', '.ts', '.tsx', '.es', '.es6', '.mjs'],
-  //       alias: {
-  //         '^@/([A-Z].*)': 'src/components/\\1',
-  //         '^@/(.*)': 'src/\\1',
-  //       },
-  //     },
-  //   ],
-  // ],
+  presets: [ "next/babel" ],
+  plugins: [],
 }
