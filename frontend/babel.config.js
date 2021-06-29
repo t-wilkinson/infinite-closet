c1a253c36e4eb518ba75f3a9288be9c64b17a74f
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-06-29 12:26:53 -0400

    [safari

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   presets: [
-    'next/babel',
-    {
-      '@babel/preset-env': {
-        targets: {
-          browsers: ['>0.03%'],
-          corejs: { version: 3, proposals: true },
+    [
+      'next/babel',
+      {
+        '@babel/preset-env': {
+          targets: {
+            browsers: ['>0.03%'],
+            corejs: { version: 3, proposals: true },
+          },
+          useBuiltIns: 'entry',
         },
-        useBuiltIns: 'entry',
       },
-    },
+    ],
   ],
   plugins: [],
 }
