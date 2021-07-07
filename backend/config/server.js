9e2408aa2b2407b9cfd4d02d1372d2f7c3e286f4
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-07-07 20:27:18 +0000

    [feat]

--- backend/config/server.js
+++ backend/config/server.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
   admin: {
     host: env("HOST", "0.0.0.0"),
     autoOpen: false,
-    url: '/admin',
+		url: '/admin',
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ec27516c3ddbb82286803e7d5308c81b"),
     },
