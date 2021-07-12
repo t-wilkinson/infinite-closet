438ece93697c6574761ce49952a943572599c33c
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-07-12 11:07:58 -0400

    [feat] Protect endpoints

--- frontend/src/components/User/Protected.tsx
+++ frontend/src/components/User/Protected.tsx
@@ -1,15 +1,11 @@
-import React from 'react'
-
 import { useSelector } from '@/utils/store'
 
 export const useProtected = () => {
   const user = useSelector((state) => state.user.data)
 
-  React.useEffect(() => {
-    if (user === null) {
-      window.location.pathname = '/account/signin'
-    }
-  }, [user])
+  if (user === null) {
+    window.location.pathname = '/account/signin'
+  }
 
   return user
 }
