1328db1a5df34b80ad9e3f8af1346e1f7563f775
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-07-21 08:41:09 -0400

    [bug] Add hidden fields on creation

--- backend/api/product/models/product.js
+++ backend/api/product/models/product.js
@@ -3,46 +3,53 @@
 const _ = require('lodash')
 const models = require('../../../data/data.js').models
 
-module.exports = {
-  lifecycles: {
-    async beforeUpdate(params, data) {
-      // Don't allow modifying hidden `model` fields
-      _.omit(
-        data,
-        Object.keys(models).map((v) => `${v}_`)
-      )
+async function addHiddenFields(data) {
+  // Don't allow modifying hidden `model` fields
+  _.omit(
+    data,
+    Object.keys(models).map((v) => `${v}_`)
+  )
 
-      for (const [filter, model] of Object.entries(models)) {
-        if (!(filter in data)) {
-          continue
-        }
+  for (const [filter, model] of Object.entries(models)) {
+    if (!(filter in data)) {
+      continue
+    }
 
-        if (filter === 'sizes') {
-          const sizes = await strapi
-            .query('custom.sizes')
-            .find({ id_in: data[filter].map((v) => v.id).filter((id) => id) })
-          let slugs = new Set()
+    if (filter === 'sizes') {
+      const sizes = await strapi
+        .query('custom.sizes')
+        .find({ id_in: data[filter].map((v) => v.id).filter((id) => id) })
+      let slugs = new Set()
 
-          for (const size of sizes) {
-            if (size.sizeRange) {
-              for (const value of strapi.services.size.range(size)) {
-                slugs.add(value)
-              }
-            } else {
-              slugs.add(size.size)
-            }
+      for (const size of sizes) {
+        if (size.sizeRange) {
+          for (const value of strapi.services.size.range(size)) {
+            slugs.add(value)
           }
-
-          slugs = Array.from(slugs).join(',')
-          data[`${filter}_`] = slugs
-        } else if (filter in data) {
-          const values = await strapi
-            .query(model)
-            .find({ id_in: data[filter].filter((id) => id) })
-          const slugs = values.map((v) => v.slug).join(',')
-          data[`${filter}_`] = slugs
+        } else {
+          slugs.add(size.size)
         }
       }
+
+      slugs = Array.from(slugs).join(',')
+      data[`${filter}_`] = slugs
+    } else if (filter in data) {
+      const values = await strapi
+        .query(model)
+        .find({ id_in: data[filter].filter((id) => id) })
+      const slugs = values.map((v) => v.slug).join(',')
+      data[`${filter}_`] = slugs
+    }
+  }
+}
+
+module.exports = {
+  lifecycles: {
+    async beforeUpdate(_params, data) {
+      await addHiddenFields(data)
+    },
+    async beforeCreate(data) {
+      await addHiddenFields(data)
     },
   },
 }
