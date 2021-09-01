00edfb8b29295c32d3978925af9fe180331830f7
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-09-01 14:29:11 -0400

    [test] sizing

--- backend/tests/sizing/index.js
+++ backend/tests/sizing/index.js
@@ -1,1 +1,21 @@
-it.todo('Sizing')
+const sizing = require('../../api/sizing/services/size')
+
+describe.skip('Sizing', () => {
+  test.each([
+    ['MD', 'LG', 'MD'],
+    ['MD', undefined, 'MD'],
+  ])('Contains', (start, end, expected) => {
+    expect(
+      sizing.contains({ size: start, sizeRange: end }, expected)
+    ).toBeTruthy()
+  })
+
+  test.each([
+    ['MD', 'LG', 'SM'],
+    ['SM', undefined, 'LG'],
+  ])('Does not contain', (start, end, expected) => {
+    expect(
+      sizing.contains({ size: start, sizeRange: end }, expected)
+    ).toBeFalsy()
+  })
+})
