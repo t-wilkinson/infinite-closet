2f61114c8a2de438febbea3bd11a7c6cd9458705
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-10-06 21:06:19 -0400

    [feat] Use webpack5

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,49 +4,52 @@ const withPWA = require('next-pwa')
 // const StylelintPlugin = require('stylelint-webpack-plugin')
 // const ESLintPlugin = require('eslint-webpack-plugin')
 
-module.exports = withPWA({
-  webpack: (config, {}) => {
-    config.cache = {
-      type: 'filesystem',
-    }
+module.exports = withImages(
+  withPWA({
+    webpack5: true,
+    webpack: (config, {}) => {
+      config.cache = {
+        type: 'filesystem',
+      }
 
-    // config.plugins.push(new StylelintPlugin({ files: '**/*.css', cache: true }))
-    // config.plugins.push(new ESLintPlugin({ cache: true }))
-    config.module.rules.push({
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      options: {
-        cacheCompression: false,
-        cacheDirectory: true,
-      },
-    })
+      // config.plugins.push(new StylelintPlugin({ files: '**/*.css', cache: true }))
+      // config.plugins.push(new ESLintPlugin({ cache: true }))
+      config.module.rules.push({
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          cacheCompression: false,
+          cacheDirectory: true,
+        },
+      })
 
-    return config
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  pwa: {
-    disable: process.env.NODE_ENV !== 'production',
-    // dest: 'public',
-    mode: 'production',
-    register: true,
-    sw: '/sw.js',
-  },
-  i18n: {
-    locales: ['en-GB', 'en-US'],
-    defaultLocale: 'en-GB',
-  },
-  images: {
-    domains: [
-      'ic.com',
-      'api.ic.com',
-      'admin.ic.com',
-      'dev.' + domain,
-      'api.dev.' + domain,
-      domain,
-      'api.' + domain,
-    ],
-  },
-})
+      return config
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    pwa: {
+      disable: process.env.NODE_ENV !== 'production',
+      // dest: 'public',
+      mode: 'production',
+      register: true,
+      sw: '/sw.js',
+    },
+    i18n: {
+      locales: ['en-GB', 'en-US'],
+      defaultLocale: 'en-GB',
+    },
+    images: {
+      domains: [
+        'ic.com',
+        'api.ic.com',
+        'admin.ic.com',
+        'dev.' + domain,
+        'api.dev.' + domain,
+        domain,
+        'api.' + domain,
+      ],
+    },
+  })
+)
