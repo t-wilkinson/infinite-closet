a2920bdfab34aca711ab34a6f5be64c19b4c93b8
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-11-12 18:09:59 -0500

    [refactor] Remove unecessary routes

--- backend/api/account/controllers/mail.js
+++ backend/api/account/controllers/mail.js
@@ -2,50 +2,6 @@
 const { splitName } = require('../../utils')
 
 module.exports = {
-  // async newsletter(ctx) {
-  //   const body = ctx.request.body
-
-  //   await strapi.plugins['email'].services.email.send({
-  //     template: 'newsletter-subscription',
-  //     to: body.email,
-  //     subject: 'You Made The List!',
-  //   })
-
-  //   await strapi.query('contact').create({
-  //     contact: body.email,
-  //     context: 'newsletter',
-  //     metadata: {
-  //       email: body.email,
-  //     },
-  //   })
-
-  //   ctx.send({ ok: true })
-  // },
-
-  // async waitlist(ctx) {
-  //   const body = ctx.request.body
-
-  //   await strapi.plugins['email'].services.email.send({
-  //     template: 'waitlist-subscription',
-  //     to: { name: body.name, email: body.email },
-  //     subject: 'You subscribed to the newsletter',
-  //   })
-
-  //   await strapi.query('contact').create({
-  //     contact: body.email,
-  //     context: 'waitlist',
-  //     metadata: {
-  //       name: body.name,
-  //       email: body.email,
-  //       comment: body.comment,
-  //       subscribe: body.subscribe,
-  //       marketing: body.marketing,
-  //     },
-  //   })
-
-  //   ctx.send({ ok: true })
-  // },
-
   async mailingList(ctx) {
     const body = ctx.request.body
 
@@ -55,7 +11,6 @@ module.exports = {
     //   subject: 'You subscribed to the mailing list',
     // })
 
-    console.log('sending email')
     strapi.services.mailchimp.template('welcome-to-the-fashion-revolution', {
       subject: 'Welcome to the fashion revolution',
       to: { name: body.name, email: body.email },
