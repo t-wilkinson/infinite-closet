0d8f4e6874efaa0d634d560e4d7f6953d7edbb92
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-12-02 00:44:12 -0500

    [bug]

--- frontend/src/pages/review/products/[slug].tsx
+++ frontend/src/pages/review/products/[slug].tsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import {useRouter} from 'next/router'
 
 import Layout from '@/Layout'
 import AddReview from '@/Shop/AddReview'
 
 export const Page = () => {
-  const [router, setRouter] = React.useState(null)
-  React.useEffect(() => {
-    setRouter(useRouter())
-  }, [])
-  const slug = router?.query.slug
-
   return <Layout title="Review Your Purchases">
     <section className="max-w-screen-lg w-full">
       <AddReview />
