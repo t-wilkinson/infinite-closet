28e7f65a9fcb57fde90470f5d3f39c64d6b17bb6
t-wilkinson <winston.trey.wilkinson@gmail.com>
2021-12-02 00:35:17 -0500

    [bug]

--- frontend/src/pages/review/products/[slug].tsx
+++ frontend/src/pages/review/products/[slug].tsx
@@ -5,12 +5,15 @@ import Layout from '@/Layout'
 import AddReview from '@/Shop/AddReview'
 
 export const Page = () => {
-  const router = useRouter()
-  const slug = router.query.slug
+  const [router, setRouter] = React.useState(null)
+  React.useEffect(() => {
+    setRouter(useRouter())
+  }, [])
+  const slug = router?.query.slug
 
   return <Layout title="Review Your Purchases">
     <section className="max-w-screen-lg w-full">
-      <AddReview productSlug={slug} onSubmit={(review) => router.push('/review/thankyou')} />
+      <AddReview productSlug={slug} onSubmit={(review) => router?.push('/review/thankyou')} />
     </section>
   </Layout>
 }
