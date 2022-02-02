16cef7480dad5890efebc9aba289a2aa89b7c1e8
t-wilkinson <winston.trey.wilkinson@gmail.com>
2022-02-02 06:06:41 -0500

    [feat] Mailchimp integration

--- backend/tests/mailchimp/index.js
+++ backend/tests/mailchimp/index.js
@@ -20,29 +20,29 @@ describe('Mailchimp', () => {
   })
 
   it.only('production has cart information', async () => {
-    const carts = await mailchimp.marketing.ecommerce.getStoreCarts(ids('store', 'development'))
+    const carts = await mailchimp.marketing.ecommerce.getStoreCarts(ids('store', 'production'))
     console.log(carts)
   })
+
+  // it('syncs', async () => {
+  //   const p = strapi.services.mailchimp.sync.product
+  //   const c = strapi.services.mailchimp.sync.cart
+
+  //   const carts = await c.getLocalItems()
+  //   const productIds = new Set(
+  //     Object.values(carts).reduce((ids, orders) => {
+  //       return ids.concat(orders.map((order) => order.product.id.toString()))
+  //     }, [])
+  //   )
+
+  //   let products = await p.getLocalItems()
+  //   for (const key in products) {
+  //     if (!productIds.has(key)) {
+  //       delete products[key]
+  //     }
+  //   }
+
+  //   await strapi.services.mailchimp.sync.all(p.sync, products, await p.getMailchimpItems())
+  //   await strapi.services.mailchimp.sync.all(c.sync, carts, await c.getMailchimpItems())
+  // })
 })
-
-// it('syncs', async () => {
-//   const p = strapi.services.mailchimp.sync.product
-//   const c = strapi.services.mailchimp.sync.cart
-
-//   const carts = await c.getLocalItems()
-//   const productIds = new Set(
-//     Object.values(carts).reduce((ids, orders) => {
-//       return ids.concat(orders.map((order) => order.product.id.toString()))
-//     }, [])
-//   )
-
-//   let products = await p.getLocalItems()
-//   for (const key in products) {
-//     if (!productIds.has(key)) {
-//       delete products[key]
-//     }
-//   }
-
-//   await strapi.services.mailchimp.sync.all(p.sync, products, await p.getMailchimpItems())
-//   await strapi.services.mailchimp.sync.all(c.sync, carts, await c.getMailchimpItems())
-// })
